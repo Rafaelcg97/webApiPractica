e280d5acdc55fd620a216ebebf31c5d2e19b8588
RafaelCastro <Dr Pc@DESKTOP-EBBEO9H>
2023-02-25 13:06:35 -0600

    practica02

--- webApiPractica/Controllers/equiposController.cs
+++ webApiPractica/Controllers/equiposController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using webApiPractica.Models;
 
 namespace webApiPractica.Controllers
@@ -43,5 +44,77 @@ namespace webApiPractica.Controllers
             return Ok(equipo);
         }
 
+        [HttpGet]
+        [Route("Find/{filtro}")]
+        public IActionResult FinByDescription(string filtro)
+        {
+            equipos? equipo = (from e in _equiposContext.equipos
+                               where e.descripcion.Contains(filtro)
+                               select e).FirstOrDefault();
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+            return Ok(equipo);
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult GuardarEquipo([FromBody] equipos equipo)
+        {
+            try
+            {
+                _equiposContext.equipos.Add(equipo);
+                _equiposContext.SaveChanges();
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        
+        [HttpPut]
+        [Route("actualizar/{id}")]
+        public IActionResult ActualizarEquipo(int id, [FromBody] equipos equipoModificar) {
+            equipos? equipoActual = (from e in _equiposContext.equipos
+                               where e.id_equipos == id
+                               select e).FirstOrDefault();
+            if (equipoActual == null)
+            {
+                return NotFound();
+            }
+
+            equipoActual.nombre = equipoModificar.nombre;
+            equipoActual.descripcion = equipoModificar.descripcion;
+            equipoActual.marca_id = equipoModificar.marca_id;
+            equipoActual.tipo_equipo_id = equipoModificar.tipo_equipo_id;
+            equipoActual.anio_compra = equipoModificar.anio_compra;
+            equipoActual.costo = equipoModificar.costo;
+
+            _equiposContext.Entry(equipoActual).State=EntityState.Modified;
+            _equiposContext.SaveChanges();
+
+            return Ok(equipoModificar);
+
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public IActionResult EliminarEquipo(int id)
+        {
+            equipos? equipo = (from e in _equiposContext?.equipos
+                               where e.id_equipos == id
+                               select e).FirstOrDefault();
+            if(equipo == null)
+                return NotFound();
+
+            _equiposContext.equipos.Attach(equipo);
+            _equiposContext.equipos.Remove(equipo);
+            _equiposContext.SaveChanges();
+
+            return Ok(equipo);
+        }
+        
     }
 }
